32c1cda6f4ef2e73e9afbbb2d8f8a9c3aed1e6fd
lazynisha <158816786+lazynisha@users.noreply.github.com>
2024-02-04 19:19:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "20417540"))
+  API_HASH = os.environ.get("API_HASH", "00d73c3d996a5b63f9fb999fb526cec4")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6464175330:AAGxRB0AOis3tZTp2DBA1Hbdl5AvSZzCXGU")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "lazynishabot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002044952858"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "paisakamalo.com")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "22c79736940cabb6048ac6f02a8b10d19c585b11")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6288746313"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://lazynishatc:85yCb8eahNNv7VC6@lazynisha.g4gwfir.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001993453001")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002045652808")
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -23,7 +23,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](https://t.me/{lazynishabot"})
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
